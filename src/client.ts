1b506253276a25abf31b7c06099b35780861c404
mostafa <14712022100624@stud.cu.edu.eg>
2025-07-10 21:40:28 +0300

    feat: finish supported commands

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,11 @@
 import net, { Socket } from 'net'
 import { encodeCommand } from '../utilis/commandEncoding.js';
 export type ServerType = 'memo:master' | 'memo:replica'
+function processBuffer(buffer: Buffer) {
+  while (true) {
+    //TODO
+  }
+}
 export function createClient(servertype?: ServerType) {
   let connectionClient: Socket;
   if (!servertype || servertype === 'memo:master')
@@ -10,7 +15,9 @@ export function createClient(servertype?: ServerType) {
 
   const pendingResolvers: ((data: string) => void)[] = [];
 
+  let buffer = Buffer.alloc(0);
   connectionClient.on('data', (data) => {
+    buffer = Buffer.concat([buffer, data])
     const res = data.toString().trim();
     const resolver = pendingResolvers.shift();
     if (resolver) {
@@ -34,6 +41,65 @@ export function createClient(servertype?: ServerType) {
     const command = ['GET', key]
     return sendCommand(command)
   }
+  function del(keys: string[]) {
+    const command = ['DEL', ...keys];
+    return sendCommand(command)
+  }
+  function expire(key: string, noSecondsTillExpire: number) {
+    const command = ['EXPIRE', key, noSecondsTillExpire.toString()]
+    return sendCommand(command)
+  }
+  function info() {
+    const command = ['INFO']
+    return sendCommand(command)
+  }
+  function ping() {
+    const command = ['PING']
+    return sendCommand(command)
+  }
+  function multi() {
+    const commandsQueue: string[][] = [];
+    commandsQueue.push(['MULTI'])
+    const multiApi = {
 
-  return { set, get, sendCommand }
+      set(key: string, value: string) {
+        commandsQueue.push(['SET', key, value]);
+        return multiApi;
+      },
+      get(key: string) {
+        commandsQueue.push(["GET", key])
+        return multiApi
+      },
+      del(keys: string[]) {
+        commandsQueue.push(['DEL', ...keys]);
+        return multiApi;
+      },
+      expire(key: string, noSecondsTillExpire: number) {
+        commandsQueue.push(['EXPIRE', key, noSecondsTillExpire.toString()]);
+        return multiApi
+      },
+
+      info() {
+        commandsQueue.push(['INFO'])
+        return multiApi;
+      },
+      ping() {
+        commandsQueue.push(['PING'])
+        return multiApi
+      },
+      async exec() {
+        for (const command of commandsQueue) {
+          await sendCommand(command)
+        }
+        return sendCommand(['EXEC'])
+      },
+      discard() {
+        return sendCommand(['DISCARD'])
+      }
+    }
+    return multiApi;
+  }
+
+  return { set, get, del, expire, info, ping, multi, sendCommand }
 }
+
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-import { createClient } from './client.js';
-const client = createClient();
+import { createClient } from "./client.js";
+
+const client = createClient()
+
+const data = await client.multi().set('hello', 'helloval').get('jkjkjvl').set('jkjkjvl', 'vajk').exec()
+
+console.log(data)
+
