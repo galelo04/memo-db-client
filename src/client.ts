161ac3dedc88dd779330278004dafb4735c3e445
mostafa <14712022100624@stud.cu.edu.eg>
2025-07-10 15:57:10 +0300

    feat: implement initial client with resolvers queue

---
+++ src/client.ts
@@ -0,0 +1,39 @@
+import net, { Socket } from 'net'
+import { encodeCommand } from '../utilis/commandEncoding.js';
+export type ServerType = 'memo:master' | 'memo:replica'
+export function createClient(servertype?: ServerType) {
+  let connectionClient: Socket;
+  if (!servertype || servertype === 'memo:master')
+    connectionClient = net.createConnection({ port: 6379 });
+  else
+    connectionClient = net.createConnection({ port: 6380 });
+
+  const pendingResolvers: ((data: string) => void)[] = [];
+
+  connectionClient.on('data', (data) => {
+    const res = data.toString().trim();
+    const resolver = pendingResolvers.shift();
+    if (resolver) {
+      resolver(res);
+    }
+  });
+  async function sendCommand(args: string[]): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const encodedCommand = encodeCommand(args);
+      connectionClient.write(encodedCommand);
+      pendingResolvers.push(resolve);
+    })
+  }
+
+  function set(key: string, value: string) {
+
+    const command = ['SET', key, value]
+    return sendCommand(command)
+  }
+  function get(key: string) {
+    const command = ['GET', key]
+    return sendCommand(command)
+  }
+
+  return { set, get, sendCommand }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,2 @@
-import readline from 'readline/promises'
-import net from 'net'
-import { encodeCommand } from '../utilis/commandEncoding.js';
-import minimist from 'minimist';
-
-const argv = minimist(process.argv.slice(2))
-
-const client = net.createConnection({ port: Number(argv.port) | 6379 });
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-async function askAndSend() {
-  const answer = await rl.question("> ")
-  const splited = answer.split(/\s+/).filter(Boolean);
-  let encoded = encodeCommand(splited)
-  client.write(encoded)
-}
-
-client.on('connect', () => {
-  askAndSend()
-})
-client.on('data', (data) => {
-  console.log(data.toString())
-  askAndSend()
-})
+import { createClient } from './client.js';
+const client = createClient();
